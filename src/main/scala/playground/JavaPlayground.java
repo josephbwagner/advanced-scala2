76186810434cf28015d67d058f1b5d8cf172002c
Joseph Wagner <joseph.wagner@hpe.com>
2022-09-12 12:17:53 -0400

    Add project files

---
+++ src/main/scala/playground/JavaPlayground.java
@@ -0,0 +1,7 @@
+package org.joseph.advscala2.playground;
+
+public class JavaPlayground {
+    public static void main(String[] args) {
+        System.out.println("Java Playground");
+    }
+}
---
+++ src/main/scala/playground/ScalaPlayground.scala
@@ -0,0 +1,6 @@
+package org.joseph.advscala2
+package playground
+
+object ScalaPlayground extends App {
+  println("Scala Playground")
+}
